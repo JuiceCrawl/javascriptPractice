447eb2089a2b7af1d8ba58a1fe2b2337d1aed0a0
Anna Garcia <annagarcia@Annas-MacBook-Pro.local>
2016-01-06 18:30:25 -0500

    Write a method that takes in an integer (greater than one) and returns true if it is prime, otherwise return false.

---
+++ isPrime.js
@@ -0,0 +1,19 @@
+/* Write a method that takes in an integer (greater than one) and returns true if it is 
+prime, otherwise return false. */
+
+/*Check for any numbers between 2 and 1 before itself for positive divisors.
+If we find at least one we return false. Otherwise after looping we retrun true. */
+function is_prime(number){
+    for(var i = 2; i < number; i++){
+        if(number % i === 0){
+            return false
+        }
+    }
+    return true
+}
+
+
+//is_prime(2) //== true
+//is_prime(3) //== true
+//is_prime(4) //== false
+//is_prime(9) //== false
