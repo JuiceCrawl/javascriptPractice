d6c57dd47abb35dbba5ac37e03a99d9d5f10d62c
Anna Garcia <annagarcia@Annas-MacBook-Pro.local>
2016-01-06 18:33:44 -0500

    added case for numbers <= 1.

--- isPrime.js
+++ isPrime.js
@@ -4,6 +4,11 @@ prime, otherwise return false. */
 /*Check for any numbers between 2 and 1 before itself for positive divisors.
 If we find at least one we return false. Otherwise after looping we retrun true. */
 function is_prime(number){
+    
+    if(number <= 1){
+        return false
+    }
+    
     for(var i = 2; i < number; i++){
         if(number % i === 0){
             return false
