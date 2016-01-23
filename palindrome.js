496cae447b96bfe1f57d206bdfd1159543a054eb
JuiceCrawl <annaegarcia@gmail.com>
2016-01-23 17:31:38 -0500

    consolidate palindrome problem and add another approach

--- palindrome.js
+++ palindrome.js
@@ -1,15 +1,30 @@
+//Palindrome
+//Using the JavaScript language, have the function Palindrome(str) take the str parameter being passed and
+//return the string true if the parameter is a palindrome, (the string is the same forward as it is backward)
+//otherwise return the string false. For example: "racecar" is also "racecar" backwards. Punctuation and
+//numbers will not be part of the string. 
+
 function palindrome(str) {
 
     var reversed = str.toUpperCase().split("").reverse().join("");
-    var str = str.toUpperCase();
+    var original = str.toUpperCase();
  
     
-    if(reversed === str){
-        return "true";
-    }else{
-        return "false";
-    }
+    return (reversed === original)
     
 }
 
 palindrome("racecar");
+
+
+/* Another option using a loop and without arrays*/
+
+// function reverseString(str) {
+//  var result = "";
+//   for(var i = 0 ; i < str.length; i++) {
+//     result = str.charAt(i) + result;
+//   }
+//   return result;
+// }
+
+// reverseString("abc")
